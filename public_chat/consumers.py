5f844e69116ad344a491e600eb46ee0e65fe7ded
Anurag Timilsina <anurag.timilsina@gmail.com>
2022-03-03 22:20:07 +0545

    Chatroom completed!

--- public_chat/consumers.py
+++ public_chat/consumers.py
@@ -1,16 +1,15 @@
 # Consumer in web socket is more like a regular django view.
 
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-class ChatRoomConsumer(AsyncJsonWebsocketConsumer):
-    async def connect(self):
 
+class ChatRoomConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         # Extracting room_name from socket url:
-        self.room_name = self.scope['url_route']['kwargs']['room_name'] 
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        
-        # adding the groups using the channels: 
+
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -18,22 +17,21 @@ class ChatRoomConsumer(AsyncJsonWebsocketConsumer):
 
         await self.accept()
 
-        # After the group is created,
-        # We might want to send confirmation!
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'tester_message',
-                'tester': 'Hi all!',
-            }
-        )
+        #     #     # Trying out tester message:
+    #     #     await self.channel_layer.group_send(
+    #     #         self.room_group_name,
+    #     #         {
+    #     #             'type': 'tester_message',
+    #     #             'tester': 'Hi all!',
+    #     #         }
+    #     #     )
 
-    async def tester_message(self, event):
-        tester = event['tester']
+    #     # async def tester_message(self, event):
+    #     #     tester = event['tester']
 
-        await self.send(text_data=json.dumps({
-            'tester': tester,
-        }))
+    #     #     await self.send(text_data=json.dumps({
+    #     #         'tester': tester,
+    #     #     }))
     
 
     async def disconnect(self, close_code):
@@ -41,3 +39,27 @@ class ChatRoomConsumer(AsyncJsonWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
+    # handling chat input from the frontend:
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        username = text_data_json['username']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chatroom_message',
+                'message': message,
+                'username': username,
+            }
+        )
+
+    async def chatroom_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+        }))
