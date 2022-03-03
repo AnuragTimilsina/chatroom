e574fba319ebe73a56677871ee8eafbf3ad1bdee
Anurag Timilsina <anurag.timilsina@gmail.com>
2022-03-03 20:39:59 +0545

    Finished with the websockets part

--- chatroom/routing.py
+++ chatroom/routing.py
@@ -1,3 +1,5 @@
+# This file is more like a configuration to setup the websocket urls insted of regular urls.
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import public_chat.routing
@@ -6,7 +8,7 @@ application = ProtocolTypeRouter({
         # Wrapping websocket with authentication.  
        'websocket': AuthMiddlewareStack(
            URLRouter(
-               public_chat.routing.websocket_urlpatterns
+               public_chat.routing.websocket_urlpatterns # Defining the url_patterns for websocket.
            )
        ),
 })
--- public_chat/consumers.py
+++ public_chat/consumers.py
@@ -1,5 +1,41 @@
-from channels.generic.websocket import AsyncJsonWebsocketConsumer
+# Consumer in web socket is more like a regular django view.
 
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+import json
 
 class ChatRoomConsumer(AsyncJsonWebsocketConsumer):
-    pass
+    async def connect(self):
+
+        # Extracting room_name from socket url:
+        self.room_name = self.scope['url_route']['kwargs']['room_name'] 
+        self.room_group_name = 'chat_%s' % self.room_name
+        
+        # adding the groups using the channels: 
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        # After the group is created,
+        # We might want to send confirmation!
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'tester_message',
+                'tester': 'Hi all!',
+            }
+        )
+
+    async def tester_message(self, event):
+        tester = event['tester']
+
+        await self.send(text_data=json.dumps({
+            'tester': tester,
+        }))
+    
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
